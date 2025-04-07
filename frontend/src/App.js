5c5b39a330dd6606e2ed5eff08502935c65bb624
Toqa <tmouhame@my.centennialcollege.ca>
2025-04-07 16:01:49 -0400

    unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { CssBaseline, Container } from '@mui/material';
-import aiImage from "../src/assets/ai-image.jpg";
 import FeatureInputForm from "./components/FeatureInputForm";
 
 function App() {
